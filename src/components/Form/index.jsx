4d125a2a609ff3572253d804082149fd16ad8e5c
discovery126 <den4ikus03@mail.ru>
2025-04-05 00:27:59 +0300

    Add Form component with basic styling and props

---
+++ src/components/Form/index.jsx
@@ -0,0 +1,46 @@
+import { useState } from "react";
+import s from "./index.module.scss";
+
+const Form = ({ title, onSubmit, fields, buttonText, children }) => {
+  const [values, setValues] = useState(
+    Object.fromEntries(fields.map((field) => [field.name, ""]))
+  );
+
+  const handleChange = (e) => {
+    setValues({ ...values, [e.target.name]: e.target.value });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log("Сработала кнопка формы");
+    onSubmit(values);
+  };
+
+  return (
+    <form className={s["form"]} onSubmit={handleSubmit}>
+      <h1 className={s["title"]}>{title}</h1>
+      {fields.map(({ name, type, placeholder, label }) => (
+        <div key={name} className={s["field"]}>
+          <label htmlFor={name} className={s["label"]}>
+            {label}
+          </label>
+          <input
+            className={s["input"]}
+            key={name}
+            type={type}
+            name={name}
+            value={values[name]}
+            onChange={handleChange}
+            placeholder={placeholder}
+          />
+        </div>
+      ))}
+      <button className={s["btn_submit"]} type="submit">
+        {buttonText || title}
+      </button>
+      {children}
+    </form>
+  );
+};
+
+export { Form };
