b646018e1384d05f29573d83596b2a8cf83d2d50
discovery126 <den4ikus03@mail.ru>
2025-04-08 17:02:10 +0300

    Added jwt-token expiration date check in the interceptor

--- src/http/index.jsx
+++ src/http/index.jsx
@@ -1,17 +1,32 @@
 import axios from "axios";
-
+import { jwtDecode } from "jwt-decode";
+import { removeUser } from "../store/slices/authSlice";
+import store from "../store/"
 export const API_URL = "http://localhost:8080/api/v1";
 
 const $api = axios.create({
   baseURL: API_URL,
 });
 
-$api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-  if (token) {
-    config.headers.Authorization = `Bearer ${localStorage.getItem("token")}`;
+$api.interceptors.request.use(
+  (config) => {
+    const token = localStorage.getItem("token");
+    if (token) {
+      const decoded = jwtDecode(token);
+      const tokenExpiration = decoded.exp;
+
+      const currentTime = Math.floor(Date.now() / 1000);
+      if (tokenExpiration < currentTime) {
+        localStorage.removeItem("token");
+        store.dispatch(removeUser());
+      }
+      config.headers["Authorization"] = `Bearer ${token}`;
+    }
+    return config;
+  },
+  (error) => {
+    return Promise.reject(error);
   }
-  return config;
-});
+);
 
 export default $api;
