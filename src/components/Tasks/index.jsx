c0ef8d2d96115b28d238371a69c5b865f75d07c7
discovery126 <den4ikus03@mail.ru>
2025-04-11 20:29:24 +0300

    Changes in card tasks and container activity tasks sizes

--- src/components/Tasks/index.jsx
+++ src/components/Tasks/index.jsx
@@ -22,49 +22,51 @@ const Tasks = ({ tasks, refreshTasks }) => {
   };
 
   return (
-    <div className={s["task-list"]}>
-      {tasks.length === 0 ? (
-        <div className={s["no-tasks-message"]}>
-          Нет заданий в этой категории
-        </div>
-      ) : (
-        tasks.map((task) => (
-          <div
-            key={task.id}
-            onClick={() => {
-              setSelectedTask(task);
-              setModalTaskActive(true);
-            }}
-            className={s["task-card"]}
-          >
-            <div className={s["task-card__header"]}>
-              <div className={s["task-card__category"]}>
-                {task.category.category_name}
-              </div>
-              <div className={s["task-card__timer"]}>
-                <CountdownTimer expiredDate={getExpiredDate(task)} />
-              </div>
-              {selectType(task)}
-            </div>
-            <div className={s["task-card__body"]}>
-              <div className={s["task-card__title"]}>{task.title}</div>
-              <div className={s["task-card__points"]}>
-                {formatPoints(task.points)}
-              </div>
-            </div>
-            <div className={s["task-card__description"]}>
-              {truncateText(task.description, 60)}
-            </div>
-          </div>
-        ))
-      )}
+    <>
       <TaskModal
         task={selectedTask}
         modalTaskActive={modalTaskActive}
         setModalTaskActive={setModalTaskActive}
         refreshTasks={refreshTasks}
-      />
-    </div>
+      />{" "}
+      <div className={s["task-list"]}>
+        {tasks.length === 0 ? (
+          <div className={s["no-tasks-message"]}>
+            Нет заданий в этой категории
+          </div>
+        ) : (
+          tasks.map((task) => (
+            <div
+              key={task.id}
+              onClick={() => {
+                setSelectedTask(task);
+                setModalTaskActive(true);
+              }}
+              className={s["task-card"]}
+            >
+              <div className={s["task-card__header"]}>
+                <div className={s["task-card__category"]}>
+                  {task.category.category_name}
+                </div>
+                <div className={s["task-card__timer"]}>
+                  <CountdownTimer expiredDate={getExpiredDate(task)} />
+                </div>
+                {selectType(task)}
+              </div>
+              <div className={s["task-card__body"]}>
+                <div className={s["task-card__title"]}>{task.title}</div>
+                <div className={s["task-card__points"]}>
+                  {formatPoints(task.points)}
+                </div>
+              </div>
+              <div className={s["task-card__description"]}>
+                {truncateText(task.description, 40)}
+              </div>
+            </div>
+          ))
+        )}
+      </div>
+    </>
   );
 };
 
